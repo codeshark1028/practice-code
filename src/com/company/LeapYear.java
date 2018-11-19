41ee928a8c36bb792cc0f41287ed0708abeaeef6
Michael <giffordmichael1@gmail.com>
2018-11-19 16:53:20 -0700

    Added LeapYear and Main classes to project.

---
+++ src/com/company/LeapYear.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class LeapYear {
+
+    public static boolean isLeapYear(int year) {
+
+        if ((year >= 1 && year <= 9999) && (year % 4 == 0 || year % 400 == 0 || year % 100 != 0)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+
+}
